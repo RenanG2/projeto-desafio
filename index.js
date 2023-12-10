a4122cfeb9c9c11e80dd22c1d4f0e683c01d016d
RenanG2 <renan.teixeirag@gmail.com>
2023-12-10 14:50:49 -0300

    projeto pronto

---
+++ index.js
@@ -0,0 +1,23 @@
+let nome = "Renan"
+let xp = 4000
+let rank = ""
+
+if (xp <= 1000) {
+    rank = "Ferro"
+} else if (xp <= 2000) {
+    rank = "Bronze"
+} else if (xp <= 5000) {
+    rank = "Prata"
+} else if (xp <= 7000) {
+    rank = "Ouro"
+} else if (xp <= 8000) {
+    rank = "Platina"
+} else if (xp <= 9000) {
+    rank = "Ascendente"
+} else if (xp <= 10000) {
+    rank = "Imortal"
+} else if (xp > 10000) {
+    rank = "Radiante"
+}
+
+console.log(`O Herói de nome ${nome} está no nível de ${rank}`)
